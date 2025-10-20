6eb9bde82e7862b882121c99a9714be07fc40fb5
TheDaniel27 <angeldanieljimenez321@gmail.com>
2025-10-19 23:33:34 -0600

    user_historial

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -7,6 +7,10 @@ use App\Models\Encriptados;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth; 
+
+
+
 
 class HistoryController extends Controller
 {
@@ -15,7 +19,7 @@ class HistoryController extends Controller
     $inputs = $request->validated();
 
     $query = Encriptados::select('id', 'user_id', 'content', 'created_at')
-                ->where('user_id', auth()->id());
+                ->where('user_id', Auth::id())
     Log::debug($request->query('search'));
     if (!empty($request->query('search'))) {
         $query->where('content', 'like', '%' . $request->query('search') . '%');
